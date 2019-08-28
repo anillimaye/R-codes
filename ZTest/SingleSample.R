2866fca53fd8263e84fc8b31cd206bf4e5a65bd4
Anil Limaye <amul@iitg.ac.in>
2019-08-28 16:31:16 +0530

    added annotations to the code

--- ZTest/SingleSample.R
+++ ZTest/SingleSample.R
@@ -1,10 +1,12 @@
-# One sample z-test
-# Takes a column of values as input and computes mean. Performs a z test to test whether the sample mean is significantly different from a known standard.
-Data <- read.table(file.choose(), header=TRUE, sep="\t")
-stdval <- 50
-SE = sd(Data$measurements)/sqrt(length(Data$measurements))
-d = (mean(Data$measurements)-stdval)/SE
-p = 2*pnorm(d, mean=0, sd=1, lower.tail=FALSE)
+# This code is for one sample z-test
+# Takes a column of values as input and computes mean. Performs a z test to test whether the sample mean is significantly different from a known standard. This code will work on a tab delimited file which has a column of a given number of measurements. The column header is "measurements".
+# Note that in this case, the sample size is large
+Data <- read.table(file.choose(), header=TRUE, sep="\t")   #reads the table which contains data
+stdval <- 50						   #here the mean is being compared to a standard value of 50
+SE = sd(Data$measurements)/sqrt(length(Data$measurements)) #computing standard error
+d = (mean(Data$measurements)-stdval)/SE			   #computing test statistic - d
+p = 2*pnorm(d, mean=0, sd=1, lower.tail=FALSE)		   #computing the p-value for the statistic
+# Final conclusion of the statistical test
 if (p < 0.05) { 
   print ("significantly different")
 } else {
