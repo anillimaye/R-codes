b6e1b37deaf94e72a16ef6d769b7c518e2ff574b
anillimaye <54581287+anillimaye@users.noreply.github.com>
2019-08-29 11:01:51 +0530

    Delete AnovaTukeyHSD.R

--- AnovaTukeyHSD.R
+++
@@ -1,9 +0,0 @@
-Data <- read.table(file.choose(), header=TRUE, sep="\t")
-Data
-summary(Data)
-boxplot(Data)
-Stacked_Data <- stack(Data)
-Stacked_Data
-Anova_result <- aov(values~ind, data=Stacked_Data)
-summary(Anova_result)
-TukeyHSD(Anova_result)
