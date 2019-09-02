26fb9925148a8450ea41ea023be897e5f67d7802
Anil Limaye <54581287+anillimaye@users.noreply.github.com>
2019-09-02 12:17:09 +0530

    Update in annotation

--- ZTest/SingleSample.R
+++ ZTest/SingleSample.R
@@ -1,5 +1,5 @@
 # This code is for one sample z-test
-# Takes a column of values as input and computes mean. Performs a z test to test whether the sample mean is significantly different from a known standard. This code will work on a tab delimited file which has a column of a given number of measurements. The column header is "measurements".
+# Takes a column of values as input and computes mean. Performs a two-tailed z test to test whether the sample mean is significantly different from a known standard. This code will work on a tab delimited file which has a column of a given number of measurements. The column header is "measurements".
 # Note that in this case, the sample size is large
 Data <- read.table(file.choose(), header=TRUE, sep="\t")   #reads the table which contains data
 stdval <- 50						   #here the mean is being compared to a standard value of 50
